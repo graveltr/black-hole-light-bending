0265afcdb617e8615216c3a6504372a73168a966
Trevor Gravely <trevor.gravely@vanderbilt.edu>
2024-05-15 21:53:26 -0500

    Refactored for cleanliness.

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 import * as THREE from 'three';
+import { parseCSV } from './utilities.js';
 
-function getRandomBoolean() {
-  return Math.random() >= 0.5;
+const CAPTUREON = import.meta.env.VITE_CAPTUREON;
+const CAPTURESECONDS = import.meta.env.VITE_CAPTURESECONDS;
+const MOVIENUMBER = import.meta.env.VITE_MOVIENUMBER;
+const MAXPOINTS = 10000;
+
+const colors = [];
+if (MOVIENUMBER == 1) {
+  colors.push(0xff0000);
+  colors.push(0x00ff00);
+  colors.push(0x0000ff);
+} else if (MOVIENUMBER == 2) {
+  colors.push(0xff0000);
+  colors.push(0xff0000);
+  colors.push(0x00ff00);
+  colors.push(0x00ff00);
+  colors.push(0x0000ff);
+  colors.push(0x0000ff);
+} else {
+  throw new Error('Unknown movie number!');
 }
 
-function getRandomFloatOutsideInterval(halfWidth, maxDist) {
-    const dist = (maxDist/2.0) + THREE.MathUtils.randFloatSpread(maxDist);
-    return getRandomBoolean() ? -1 * halfWidth - dist : halfWidth + dist;
+const csvUrls = [];
+if (MOVIENUMBER == 1) {
+  csvUrls.push('trajectories/subsupercritical/cameraTrajectory.csv');
+  csvUrls.push('trajectories/subsupercritical/ray1.csv');
+  csvUrls.push('trajectories/subsupercritical/ray2.csv');
+  csvUrls.push('trajectories/subsupercritical/ray3.csv');
+} else if (MOVIENUMBER == 2) {
+  csvUrls.push('trajectories/pairwise/cameraTrajectory.csv');
+  csvUrls.push('trajectories/pairwise/ray1.csv');
+  csvUrls.push('trajectories/pairwise/ray2.csv');
+  csvUrls.push('trajectories/pairwise/ray3.csv');
+  csvUrls.push('trajectories/pairwise/ray4.csv');
+  csvUrls.push('trajectories/pairwise/ray5.csv');
+  csvUrls.push('trajectories/pairwise/ray6.csv');
+} else {
+  throw new Error('Unknown movie number!');
 }
-
-function sphericalToCartesian(r, theta, phi) {
-  const x = r * Math.sin(theta) * Math.cos(phi);
-  const y = r * Math.sin(theta) * Math.sin(phi);
-  const z = r * Math.cos(theta);
-
-  return { x, y, z };
-}
-
-// Function to parse CSV text into a matrix
-function parseCSV(csvText) {
-  return csvText.trim().split('\n').map(row => row.split(','));
-}
-
-// Array of CSV file URLs
-/* const csvUrls = [
-  'trajectories/subsupercritical/cameraTrajectory.csv',
-  'trajectories/subsupercritical/ray1.csv',
-  'trajectories/subsupercritical/ray2.csv',
-  'trajectories/subsupercritical/ray3.csv'
-]; */
-
-const csvUrls = [
-  'trajectories/pairwise/cameraTrajectory.csv',
-  'trajectories/pairwise/ray1.csv',
-  'trajectories/pairwise/ray2.csv',
-  'trajectories/pairwise/ray3.csv',
-  'trajectories/pairwise/ray4.csv',
-  'trajectories/pairwise/ray5.csv',
-  'trajectories/pairwise/ray6.csv',
-];
 
 // Map over the URLs and return an array of promises
 const fetchPromises = csvUrls.map(url =>
@@ -49,7 +49,6 @@ const fetchPromises = csvUrls.map(url =>
 Promise.all(fetchPromises)
   .then(csvTexts => {
     // csvTexts is an array containing the contents of each CSV file as text
-    // You can now parse the CSV text and process the data as needed
     const trajectories = csvTexts.map(csvText => parseCSV(csvText));
     process(trajectories[0], trajectories.slice(1));
   })
@@ -62,100 +61,24 @@ function process(cameraTrajectory, rayTrajectories) {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
   const cameraCenter = new THREE.Vector3(13, 0, 0);
-
   const renderer = new THREE.WebGLRenderer();
   const textureLoader = new THREE.TextureLoader();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  const blackholeMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(13, 64, 64), 
-    new THREE.MeshBasicMaterial({ color: 0x000000 })
-  );
-  scene.add(blackholeMesh);
+  // Add all of the scene elements
+  addBlackHole(scene);
+  addSpinAxis(scene);
+  addSkyDome(scene, textureLoader);
+  const rayMeshes = addRays(scene, rayTrajectories.length);
+  const [trails, currPoints] = addTrails(scene, rayTrajectories.length);
+  if(MOVIENUMBER == 2) { addRadialAxis(scene); }
 
-  // const colors = [0xff0000, 0x00ff00, 0x0000ff];
-  const colors = [0xff0000, 0xff0000, 0x00ff00, 0x00ff00, 0x0000ff, 0x0000ff];
-  const rayMeshes = [];
-  for (let i = 0; i < rayTrajectories.length; i++){
-    const rayMesh = new THREE.Mesh(
-      new THREE.SphereGeometry(0.05, 32, 32),
-      new THREE.MeshBasicMaterial({ color: colors[i] })
-    );
-    rayMeshes.push(rayMesh);
-    scene.add(rayMesh);
-  }
-
-  const maxPoints = 10000;
-  let trails = [];
-  let currPoints = [];
-  for (let i = 0; i < rayMeshes.length; i++) {
-    const trailGeometry = new THREE.BufferGeometry();
-    const positions = new Float32Array(maxPoints * 3);
-    trailGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-    const trailMaterial = new THREE.LineBasicMaterial({ color: colors[i], linewidth: 3 });
-    const trail = new THREE.Line(trailGeometry, trailMaterial)
-    currPoints.push(0);
-    trails.push(trail);
-    scene.add(trail);
-  }
-
-  function updateTrail(object, trail, points, idx) {
-    const trailGeometry = trail.geometry;
-    const positionAttribute = trailGeometry.attributes.position;
-
-    positionAttribute.array[points[idx] * 3] = object.position.x;
-    positionAttribute.array[points[idx] * 3 + 1] = object.position.y;
-    positionAttribute.array[points[idx] * 3 + 2] = object.position.z;
-
-    points[idx] = (points[idx] + 1) % maxPoints;
-    positionAttribute.needsUpdate = true;
-
-    // If the trail is shorter than the maxPoints, set the draw range to the current number of points
-    if (points[idx] < maxPoints - 1) {
-      trailGeometry.setDrawRange(0, points[idx]);
-    } else {
-      // Once the trail has reached its maximum length, we can use the entire buffer
-      trailGeometry.setDrawRange(0, maxPoints);
-    }
-  }
-
-  const skyGeometry = new THREE.SphereGeometry(2000, 60, 40);
-  const skyTexture = textureLoader.load('eso0932a.jpg'); // Replace with the path to your sky texture
-  skyTexture.colorSpace = THREE.SRGBColorSpace;
-  const skyMaterial = new THREE.MeshBasicMaterial({
-    map: skyTexture,
-    side: THREE.BackSide
-  });
-  const skyDome = new THREE.Mesh(skyGeometry, skyMaterial);
-  scene.add(skyDome);
-  skyDome.position.set(0,0,0);
-  skyDome.rotateX(Math.PI/2.0)
-
-  const axisPoints = [];
-  axisPoints.push(new THREE.Vector3(0, 0, 15));
-  axisPoints.push(new THREE.Vector3(0, 0, -15));
-  const spinAxis = new THREE.Line(
-    new THREE.BufferGeometry().setFromPoints(axisPoints), 
-    new THREE.LineBasicMaterial({ color: 0xe1e1e1, linewidth: 3 })
-  );
-  scene.add(spinAxis)
-
-  const radialAxisPoints = [];
-  radialAxisPoints.push(new THREE.Vector3(0, 0, 0));
-  radialAxisPoints.push(new THREE.Vector3(40, 0, 0));
-  const radialAxis = new THREE.Line(
-    new THREE.BufferGeometry().setFromPoints(radialAxisPoints), 
-    new THREE.LineBasicMaterial({ color: 0xe1e1e1, linewidth: 2 })
-  );
-  scene.add(radialAxis)
-
-/*   const capturer = new CCapture({
+  const capturer = new CCapture({
     format: 'webm',
     framerate: 60
   });
-
-  capturer.start(); */
+  if(CAPTUREON == 1) { capturer.start(); }
 
   let i = 0;
   let animationId;
@@ -172,18 +95,121 @@ function process(cameraTrajectory, rayTrajectories) {
     }
 	  i += 1
 	  renderer.render( scene, camera );
-    //capturer.capture( renderer.domElement )
+
+    if(CAPTUREON == 1) { 
+      console.log('capturing!')
+      capturer.capture( renderer.domElement ); 
+    }
   }
+
   animate();
-/*   setTimeout(() => {
-    cancelAnimationFrame(animationId);
-    capturer.stop();
-    capturer.save();
-  }, 15*1000); */
+
+  if(CAPTUREON == 1) {
+    setTimeout(() => {
+      cancelAnimationFrame(animationId);
+      capturer.stop();
+      capturer.save();
+    }, CAPTURESECONDS * 1000);
+  }
+}
+
+function addRays(scene, numRays) {
+  const rayMeshes = [];
+  for (let i = 0; i < numRays; i++){
+    const rayMesh = new THREE.Mesh(
+      new THREE.SphereGeometry(0.05, 32, 32),
+      new THREE.MeshBasicMaterial({ color: colors[i] })
+    );
+    rayMeshes.push(rayMesh);
+    scene.add(rayMesh);
+  }
+
+  return rayMeshes
+}
+
+function addTrails(scene, numRays) {
+  let trails = [];
+  let currPoints = [];
+  for (let i = 0; i < numRays; i++) {
+    const trailGeometry = new THREE.BufferGeometry();
+    const positions = new Float32Array(MAXPOINTS * 3);
+    trailGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    const trailMaterial = new THREE.LineBasicMaterial({ color: colors[i], linewidth: 3 });
+    const trail = new THREE.Line(trailGeometry, trailMaterial)
+    currPoints.push(0);
+    trails.push(trail);
+    scene.add(trail);
+  }
+
+  return [trails, currPoints];
+}
+
+function addSkyDome(scene, textureLoader) {
+  const skyGeometry = new THREE.SphereGeometry(2000, 60, 40);
+  const skyTexture = textureLoader.load('eso0932a.jpg'); // Replace with the path to your sky texture
+  skyTexture.colorSpace = THREE.SRGBColorSpace;
+  const skyMaterial = new THREE.MeshBasicMaterial({
+    map: skyTexture,
+    side: THREE.BackSide
+  });
+  const skyDome = new THREE.Mesh(skyGeometry, skyMaterial);
+  skyDome.position.set(0,0,0);
+  skyDome.rotateX(Math.PI/2.0)
+  scene.add(skyDome);
+}
+
+function addBlackHole(scene) {
+  const blackholeMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(13, 64, 64), 
+    new THREE.MeshBasicMaterial({ color: 0x000000 })
+  );
+  scene.add(blackholeMesh);
+}
+
+function addRadialAxis(scene) {
+  const radialAxisPoints = [];
+  radialAxisPoints.push(new THREE.Vector3(0, 0, 0));
+  radialAxisPoints.push(new THREE.Vector3(40, 0, 0));
+  const radialAxis = new THREE.Line(
+    new THREE.BufferGeometry().setFromPoints(radialAxisPoints), 
+    new THREE.LineBasicMaterial({ color: 0xe1e1e1, linewidth: 2 })
+  );
+  scene.add(radialAxis)
+}
+
+function addSpinAxis(scene) {
+  const axisPoints = [];
+  axisPoints.push(new THREE.Vector3(0, 0, 15));
+  axisPoints.push(new THREE.Vector3(0, 0, -15));
+  const spinAxis = new THREE.Line(
+    new THREE.BufferGeometry().setFromPoints(axisPoints), 
+    new THREE.LineBasicMaterial({ color: 0xe1e1e1, linewidth: 3 })
+  );
+  scene.add(spinAxis)
+}
+
+function updateTrail(object, trail, points, idx) {
+  const trailGeometry = trail.geometry;
+  const positionAttribute = trailGeometry.attributes.position;
+
+  positionAttribute.array[points[idx] * 3] = object.position.x;
+  positionAttribute.array[points[idx] * 3 + 1] = object.position.y;
+  positionAttribute.array[points[idx] * 3 + 2] = object.position.z;
+
+  points[idx] = (points[idx] + 1) % MAXPOINTS;
+  positionAttribute.needsUpdate = true;
+
+  // If the trail is shorter than the maxPoints, set the draw range to the current number of points
+  if (points[idx] < MAXPOINTS - 1) {
+    trailGeometry.setDrawRange(0, points[idx]);
+  } else {
+    // Once the trail has reached its maximum length, we can use the entire buffer
+    trailGeometry.setDrawRange(0, MAXPOINTS);
+  }
 }
 
 
-/*   const particlesGeometry = new THREE.BufferGeometry();
+/*const particlesGeometry = new THREE.BufferGeometry();
 const starsVertices = [];
 for (let i = 0; i < 50000; i++) {
   const r = getRandomFloatOutsideInterval(200, 500);
