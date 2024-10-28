eba51b081e257dad4ec9066999939f842c6ca88d
Trevor Gravely <graveltr@umich.edu>
2024-10-28 11:31:19 -0500

    Clean up + comments

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 import * as THREE from 'three';
 import { parseCSV } from './utilities.js';
 
-console.log("hello world")
-
+// Grab environment variables which I use as CLI arguments / switches.
 const CAPTUREON = import.meta.env.VITE_CAPTUREON;
 const CAPTURESECONDS = import.meta.env.VITE_CAPTURESECONDS;
 const MOVIENUMBER = import.meta.env.VITE_MOVIENUMBER;
 const MAXPOINTS = import.meta.env.VITE_MAXPOINTS;
 
+/*
+* Depending on the movie we are rendering, we set up the various ray 
+* trajectory colors. We also point to the MMA generated ray trajectories 
+* which are stored as csv files.
+*/
 const colors = [];
-if (MOVIENUMBER == 1) {
-  colors.push(0xff0000);
-  colors.push(0x00ff00);
-  colors.push(0x48B8D0);
-} else if (MOVIENUMBER == 2) {
-  colors.push(0xff0000);
-  colors.push(0xff0000);
-  colors.push(0x00ff00);
-  colors.push(0x00ff00);
-  colors.push(0x48B8D0);
-  colors.push(0x48B8D0);
-} else if (MOVIENUMBER == 3) {
-  colors.push(0x32CD32);
-  colors.push(0x17BEBB);
-} else {
-  throw new Error('Unknown movie number!');
-}
-
 const csvUrls = [];
 if (MOVIENUMBER == 1) {
+  colors.push(0xff0000);
+  colors.push(0x00ff00);
+  colors.push(0x48B8D0);
+
   csvUrls.push('trajectories/subsupercritical/cameraTrajectory.csv');
   csvUrls.push('trajectories/subsupercritical/ray1.csv');
   csvUrls.push('trajectories/subsupercritical/ray2.csv');
   csvUrls.push('trajectories/subsupercritical/ray3.csv');
 } else if (MOVIENUMBER == 2) {
+  colors.push(0xff0000);
+  colors.push(0xff0000);
+  colors.push(0x00ff00);
+  colors.push(0x00ff00);
+  colors.push(0x48B8D0);
+  colors.push(0x48B8D0);
+
   csvUrls.push('trajectories/pairwise/cameraTrajectory.csv');
   csvUrls.push('trajectories/pairwise/ray1.csv');
   csvUrls.push('trajectories/pairwise/ray2.csv');
@@ -42,6 +39,9 @@ if (MOVIENUMBER == 1) {
   csvUrls.push('trajectories/pairwise/ray5.csv');
   csvUrls.push('trajectories/pairwise/ray6.csv');
 } else if (MOVIENUMBER == 3) {
+  colors.push(0x32CD32);
+  colors.push(0x17BEBB);
+
   csvUrls.push('trajectories/equatorial-rays/cameraTrajectory.csv');
   csvUrls.push('trajectories/equatorial-rays/ray1.csv');
   csvUrls.push('trajectories/equatorial-rays/ray2.csv');
@@ -49,15 +49,15 @@ if (MOVIENUMBER == 1) {
   throw new Error('Unknown movie number!');
 }
 
-// Map over the URLs and return an array of promises
+// Map over the URLs and return an array of promises.
 const fetchPromises = csvUrls.map(url =>
   fetch(url).then(response => response.text())
 );
 
-// Use Promise.all to wait for all fetch requests to complete
+// Use Promise.all to wait for all fetch requests to complete.
 Promise.all(fetchPromises)
   .then(csvTexts => {
-    // csvTexts is an array containing the contents of each CSV file as text
+    // csvTexts is an array containing the contents of each CSV file as text.
     const trajectories = csvTexts.map(csvText => parseCSV(csvText));
     process(trajectories[0], trajectories.slice(1));
   })
@@ -65,8 +65,13 @@ Promise.all(fetchPromises)
     console.error('Error fetching one or more CSV files:', error);
   });
 
+/*
+* This is the main body of code. It is called once all of the fetch requests
+* above complete. It is passed a camera trajectory and an array of light ray 
+* trajectories, which themselves are arrays of vectores (x,y,z coordinates).
+*/
 function process(cameraTrajectory, rayTrajectories) {
-
+  // Set up the THREE.js scene.
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
   const cameraCenter = new THREE.Vector3(0, 0, 0);
@@ -75,37 +80,46 @@ function process(cameraTrajectory, rayTrajectories) {
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  // Add all of the scene elements
+  // Add all of the scene elements.
   addBlackHole(scene);
   addSpinAxis(scene);
   addSkyDome(scene, textureLoader);
   const rayMeshes = addRays(scene, rayTrajectories.length);
   const [trails, currPoints] = addTrails(scene, rayTrajectories.length, rayTrajectories.map(matrix => matrix[0]));
-  if(MOVIENUMBER == 2) { addRadialAxis(scene); }
+  if (MOVIENUMBER == 2) { addRadialAxis(scene); }
 
   const capturer = new CCapture({
     format: 'webm',
     framerate: 60
   });
-  if(CAPTUREON == 1) { capturer.start(); }
+  if (CAPTUREON == 1) { capturer.start(); }
 
   let i = 0;
   let animationId;
   camera.up.set(0, 0, 1);
+
+  // Function that runs every frame.
   function animate() {
 	  animationId = requestAnimationFrame( animate );
 
-    camera.position.set(cameraTrajectory[i][0],cameraTrajectory[i][1],cameraTrajectory[i][2]); // Positioned 10 units above the x-y plane
+    camera.position.set(cameraTrajectory[i][0],cameraTrajectory[i][1],cameraTrajectory[i][2]); 
     camera.lookAt(cameraCenter);
 
+    /*
+    * For each light ray, update the position of the photon. Also update the corresponding 
+    * ray trail.
+    */
     for (let j = 0; j < rayMeshes.length; j++){
+      // Update the photon position.
 	    rayMeshes[j].position.set(rayTrajectories[j][i][0], rayTrajectories[j][i][1], rayTrajectories[j][i][2])
-      updateTrail(rayMeshes[j], trails[j], currPoints, j)
+
+      // Now update the trail.
+      updateTrail(rayMeshes[j], trails[j])
     }
 	  i += 1
 	  renderer.render( scene, camera );
 
-    if(CAPTUREON == 1) { 
+    if (CAPTUREON == 1) { 
       console.log('capturing!')
       capturer.capture( renderer.domElement ); 
     }
@@ -113,7 +127,7 @@ function process(cameraTrajectory, rayTrajectories) {
 
   animate();
 
-  if(CAPTUREON == 1) {
+  if (CAPTUREON == 1) {
     setTimeout(() => {
       cancelAnimationFrame(animationId);
       capturer.stop();
@@ -122,8 +136,39 @@ function process(cameraTrajectory, rayTrajectories) {
   }
 }
 
+/* 
+* Function responsible for updating the trails of each light ray.
+*/ 
+function updateTrail(object, trail) {
+  const trailGeometry = trail.geometry;
+
+  /*
+  * Obtain a reference to the the position data. Remember, it is a flat array of position data
+  * for the trail nodes, x1,y1,z1,x2,y2,z2,...
+  */
+  const positionAttribute = trailGeometry.attributes.position;
+
+  // Shift the positions back to make room for the new point.
+  for (let i = positionAttribute.count - 1; i > 0; i--) {
+    const prevIndex = (i - 1) * 3;
+    const currentIndex = i * 3;
+    positionAttribute.array[currentIndex] = positionAttribute.array[prevIndex];
+    positionAttribute.array[currentIndex + 1] = positionAttribute.array[prevIndex + 1];
+    positionAttribute.array[currentIndex + 2] = positionAttribute.array[prevIndex + 2];
+  }
+
+  // Add the object's current position as the new point at the front of the trail.
+  positionAttribute.array[0] = object.position.x;
+  positionAttribute.array[1] = object.position.y;
+  positionAttribute.array[2] = object.position.z;
+
+  // Notify Three.js that the positions need updating.
+  positionAttribute.needsUpdate = true;
+}
+
 function addRays(scene, numRays) {
   const rayMeshes = [];
+
   for (let i = 0; i < numRays; i++){
     const rayMesh = new THREE.Mesh(
       new THREE.SphereGeometry(0.05, 32, 32),
@@ -139,20 +184,35 @@ function addRays(scene, numRays) {
 function addTrails(scene, numRays, initialPositions) {
   let trails = [];
   let currPoints = [];
+
+  // Iterate through the rays.
   for (let i = 0; i < numRays; i++) {
+    // This is the THREE object that will encapsulate the trail.
     const trailGeometry = new THREE.BufferGeometry();
+
+    /*
+    * This is a flattened array that contains the position data of 
+    * the nodes in the trail: x1,y1,z1,x2,y2,z2,... 
+    * 
+    * We initialize all trail nodes to start at the initial position
+    * of the ray trajectory.
+    */
     const positions = new Float32Array(MAXPOINTS * 3);
+
     for (let j = 0; j < positions.length; j += 3) {
       positions[j] = initialPositions[i][0];
       positions[j+1] = initialPositions[i][1];
       positions[j+2] = initialPositions[i][2];
     }
+
     trailGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
     const trailMaterial = new THREE.LineBasicMaterial({ color: colors[i], linewidth: 3 });
     const trail = new THREE.Line(trailGeometry, trailMaterial)
-    currPoints.push(0);
+
     trails.push(trail);
     scene.add(trail);
+
+    currPoints.push(0);
   }
 
   return [trails, currPoints];
@@ -160,15 +220,18 @@ function addTrails(scene, numRays, initialPositions) {
 
 function addSkyDome(scene, textureLoader) {
   const skyGeometry = new THREE.SphereGeometry(2000, 60, 40);
-  const skyTexture = textureLoader.load('eso0932a.jpg'); // Replace with the path to your sky texture
+  const skyTexture = textureLoader.load('eso0932a.jpg'); 
+
   skyTexture.colorSpace = THREE.SRGBColorSpace;
   const skyMaterial = new THREE.MeshBasicMaterial({
     map: skyTexture,
     side: THREE.BackSide
   });
   const skyDome = new THREE.Mesh(skyGeometry, skyMaterial);
+
   skyDome.position.set(0,0,0);
   skyDome.rotateX(Math.PI/2.0)
+
   scene.add(skyDome);
 }
 
@@ -177,64 +240,34 @@ function addBlackHole(scene) {
     new THREE.SphereGeometry(10.4, 64, 64), 
     new THREE.MeshBasicMaterial({ color: 0x000000 })
   );
+
   scene.add(blackholeMesh);
 }
 
 function addRadialAxis(scene) {
   const radialAxisPoints = [];
+
   radialAxisPoints.push(new THREE.Vector3(0, 0, 0));
   radialAxisPoints.push(new THREE.Vector3(40, 0, 0));
+
   const radialAxis = new THREE.Line(
     new THREE.BufferGeometry().setFromPoints(radialAxisPoints), 
     new THREE.LineBasicMaterial({ color: 0xe1e1e1, linewidth: 2 })
   );
+
   scene.add(radialAxis)
 }
 
 function addSpinAxis(scene) {
   const axisPoints = [];
+
   axisPoints.push(new THREE.Vector3(0, 0, 15));
   axisPoints.push(new THREE.Vector3(0, 0, -15));
+
   const spinAxis = new THREE.Line(
     new THREE.BufferGeometry().setFromPoints(axisPoints), 
     new THREE.LineBasicMaterial({ color: 0xe1e1e1, linewidth: 3 })
   );
+  
   scene.add(spinAxis)
 }
-
-function updateTrail(object, trail, points, idx) {
-  const trailGeometry = trail.geometry;
-  const positionAttribute = trailGeometry.attributes.position;
-
-  // Shift the positions back to make room for the new point
-  for (let i = positionAttribute.count - 1; i > 0; i--) {
-    const prevIndex = (i - 1) * 3;
-    const currentIndex = i * 3;
-    positionAttribute.array[currentIndex] = positionAttribute.array[prevIndex];
-    positionAttribute.array[currentIndex + 1] = positionAttribute.array[prevIndex + 1];
-    positionAttribute.array[currentIndex + 2] = positionAttribute.array[prevIndex + 2];
-  }
-
-  // Add the object's current position as the new point at the front of the trail
-  positionAttribute.array[0] = object.position.x;
-  positionAttribute.array[1] = object.position.y;
-  positionAttribute.array[2] = object.position.z;
-
-  // Notify Three.js that the positions need updating
-  positionAttribute.needsUpdate = true;
-}
-
-
-/*const particlesGeometry = new THREE.BufferGeometry();
-const starsVertices = [];
-for (let i = 0; i < 50000; i++) {
-  const r = getRandomFloatOutsideInterval(200, 500);
-  const theta = Math.random() * Math.PI;
-  const phi = Math.random() * 2 * Math.PI;
-  const cartesianCoords = sphericalToCartesian(r, theta, phi)
-  starsVertices.push(cartesianCoords.x, cartesianCoords.y, cartesianCoords.z);
-}
-particlesGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starsVertices, 3));
-const particlesMaterial = new THREE.PointsMaterial({ color: 0xffffff });
-const starField = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(starField); */
